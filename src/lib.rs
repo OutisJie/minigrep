3b971915097e5cd4c41debb0439d45e54f34fbd8
OutisJie <1014547032@qq.com>
2020-07-31 01:36:58 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    filename: String,
+    query: String
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { filename, query })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    for line in search(&config.query, &contents) {
+        println!("match line: {}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+      if line.contains(query) {
+        results.push(line);
+      }
+    }
+
+    results
+}
